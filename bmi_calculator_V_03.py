342d0ac18e26a9c7786da570ae7313190cd70440
scuddi <157764402+scuddi@users.noreply.github.com>
2024-03-28 16:24:03 +0100

    GUI is a little better; perfectioning the GUI with frames etc. comes in V_04 of the project; basic functionalities are all given now

--- bmi_calculator_V_03.py
+++ bmi_calculator_V_03.py
@@ -5,15 +5,20 @@ customtkinter.set_default_color_theme("dark-blue")
 
 app = customtkinter.CTk()
 
-app.geometry("600x400")
+app.geometry("375x280")
 
 app.title("Wilkommen in meinem BMI-Rechner.")
 
-weight_title = customtkinter.CTkLabel(app, text = "Ihr Körpergewicht in Kilogramm: ")
-weight_entry = customtkinter.CTkEntry(app)
+hinweis_label = customtkinter.CTkLabel(master = app, text = "Hinweis: Der BMI dient lediglich einer Einschätzung,\n"
+                                                   "hat aber keinen Anspruch auf Richtigkeit.\n"
+                                                   "Sind Sie beispielsweise sehr muskolös, kann fälsch- \n"
+                                                   "lich eine Einschätzung in die Kategorie 'Übergewicht' erfolgen.")
+
+weight_title = customtkinter.CTkLabel(app, text = "Ihr Körpergewicht in Kilogramm: ", )
+weight_entry = customtkinter.CTkEntry(app, placeholder_text = "z.B.: 90.6")
 
 height_title = customtkinter.CTkLabel(app, text = "Ihre Körpergröße in Metern: ")
-height_entry = customtkinter.CTkEntry(app)
+height_entry = customtkinter.CTkEntry(app, placeholder_text = "z.B.: 1.85")
 
 def output_calc():
     input_weight = float(weight_entry.get())
@@ -39,14 +44,18 @@ output_number = ""
 
 output_field = customtkinter.CTkLabel(app, text = output)
 
-converter_button = customtkinter.CTkButton(app, text = "BMI berechnen", command = output_calc)
+converter_button = customtkinter.CTkButton(app,
+                                           text = "BMI berechnen",
+                                           command = output_calc,
+                                           hover_color = "#447ec9")
 
-weight_title.grid(row = 0, column = 0, padx = 10, pady = 10)
-weight_entry.grid(row = 0, column = 1)
-height_title.grid(row = 1, column = 0)
-height_entry.grid(row = 1, column = 1)
-converter_button.grid (row = 2, column = 0, columnspan = 2)
-output_field.grid(row = 3, column = 1, columnspan = 2)
+hinweis_label.grid(row = 0, column = 0, columnspan = 2, padx = 5, pady = 5, sticky="nsew")
+weight_title.grid(row = 1, column = 0, padx = 10, pady = 10)
+weight_entry.grid(row = 1, column = 1)
+height_title.grid(row = 2, column = 0, padx = 10, pady = 10)
+height_entry.grid(row = 2, column = 1)
+converter_button.grid (row = 3, column = 0, columnspan = 2, padx = 10, pady = 10)
+output_field.grid(row = 4, column = 0, columnspan = 2, padx = 10, pady = 10)
 
 
 app.mainloop()
@@ -67,40 +76,4 @@ spacer_1 = tk.Label(bg = "#dedede",
 spacer_2 = tk.Label(bg = "#dedede",
                     height = 1,
                     width = 72)
-
-                
-
-
-output_field = tk.Label(text = output,
-                        font = ("Calibri", 14),
-                        height = 2,
-                        width = 50,
-                        bd = 2,
-                        relief = "solid",
-                        bg = "#dedede"
-                        )
-
-hinweis_field = tk.Label(text="Hinweis: Der BMI dient lediglich einer Einschätzung,\n"
-                              "hat aber keinen Anspruch auf Richtigkeit.\n"
-                              "Sind Sie beispielsweise sehr muskolös, kann fälsch- \n"
-                              "lich eine Einschätzung in die Kategorie 'Übergewicht' erfolgen.",
-                         bg = "#c2c2c2",
-                         font = ("Calibri", 14),
-                         height = 4,
-                         width = 50)
-
-title.grid(row=0, column=0, columnspan=4)
-spacer.grid(row=1, column=0, columnspan=4)
-weight_title.grid(row=2, column=0, columnspan=2)
-weight_entry.grid(row=2, column=2, columnspan=2)
-height_title.grid(row=3, column=0, columnspan=2)
-height_entry.grid(row=3, column=2, columnspan=2)
-spacer_1.grid(row=4, column=0, columnspan=4)
-converter_button.grid(row=5, column =1, columnspan=2)
-spacer_2.grid(row=6, column=0, columnspan=4)
-output_field.grid(row=7, column=0, columnspan=4)
-hinweis_field.grid(row=8, column=0, columnspan=4)
-
-window.mainloop()
-
 """
