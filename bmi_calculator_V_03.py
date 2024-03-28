83d4d6398d146e2b3cbffc45b297343d1802025b
scuddi <157764402+scuddi@users.noreply.github.com>
2024-03-28 10:39:42 +0100

    transfered basic functionality into CustomTkinter, next step is to make the GUI more appealing

--- bmi_calculator_V_03.py
+++ bmi_calculator_V_03.py
@@ -9,20 +9,50 @@ app.geometry("600x400")
 
 app.title("Wilkommen in meinem BMI-Rechner.")
 
+weight_title = customtkinter.CTkLabel(app, text = "Ihr Körpergewicht in Kilogramm: ")
+weight_entry = customtkinter.CTkEntry(app)
+
+height_title = customtkinter.CTkLabel(app, text = "Ihre Körpergröße in Metern: ")
+height_entry = customtkinter.CTkEntry(app)
+
+def output_calc():
+    input_weight = float(weight_entry.get())
+    input_height = float(height_entry.get())
+    middle = input_weight / input_height ** 2
+    output_number = round(middle, 2)
+
+    if output_number < 18.5:
+        output = "Untergewicht"
+        output_field.configure(text= output)
+    elif output_number >= 18.5 and output_number <= 24.9:
+        output ="Normalgewicht"
+        output_field.configure(text= output)
+    elif output_number >= 25 and output_number <= 34.9:
+        output = "Übergewicht"
+        output_field.configure(text= output)
+    else:
+        output = "Starkes oder extremes Übergewicht"
+        output_field.configure(text= output)
+
+output = ""
+output_number = ""
+
+output_field = customtkinter.CTkLabel(app, text = output)
+
+converter_button = customtkinter.CTkButton(app, text = "BMI berechnen", command = output_calc)
+
+weight_title.grid(row = 0, column = 0, padx = 10, pady = 10)
+weight_entry.grid(row = 0, column = 1)
+height_title.grid(row = 1, column = 0)
+height_entry.grid(row = 1, column = 1)
+converter_button.grid (row = 2, column = 0, columnspan = 2)
+output_field.grid(row = 3, column = 1, columnspan = 2)
 
 
 app.mainloop()
 
 
-""""
-window = tk.Tk()
-
-title = tk.Label(text = "Wilkommen in meinem BMI-Rechner.",
-                 bg = "#c2c2c2",
-                 font = ("Calibri", 14),
-                 height = 4,
-                 width = 50
-                 )
+"""
 
 # spacers to make gui look less crowded
 
@@ -38,55 +68,8 @@ spacer_2 = tk.Label(bg = "#dedede",
                     height = 1,
                     width = 72)
 
-# title and input fields
+                
 
-weight_title = tk.Label(text = "Ihr Körpergewicht in Kilogramm: ",
-                        bg = "#dedede",
-                        font = ("Calibri", 14),
-                        width = 25
-                        )
-
-weight_entry = tk.Entry(font = ("Calibri",14),
-                        width = 25
-                        )
-
-height_title = tk.Label(text = "Ihre Körpergröße in Metern: ",
-                        bg = "#dedede",
-                        font = ("Calibri", 14),
-                        width = 25
-                        )
-
-height_entry = tk.Entry(font = ("Calibri", 14),
-                        width = 25
-                        )
-# function to calculate
-def output_calc():
-    input_weight = float(weight_entry.get())
-    input_height = float(height_entry.get())
-    middle = input_weight / input_height ** 2
-    output_number = round(middle, 2)
-
-    if output_number < 18.5:
-        output = "Untergewicht"
-        output_field.config(text= output)
-    elif output_number >= 18.5 and output_number <= 24.9:
-        output ="Normalgewicht"
-        output_field.config(text= output)
-    elif output_number >= 25 and output_number <= 34.9:
-        output = "Übergewicht"
-        output_field.config(text= output)
-    else:
-        output = "Starkes oder extremes Übergewicht"
-        output_field.config(text= output)
-
-
-converter_button = tk.Button(text = "BMI errechnen",
-                             font = ("Calibri", 14),
-                             command = output_calc
-                             )
-
-output = ""
-output_number = ""
 
 output_field = tk.Label(text = output,
                         font = ("Calibri", 14),
